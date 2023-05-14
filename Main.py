831fade84709c44028688f23807f9146b209fdb3
HummerJohn <olavbolet@gmail.com>
2023-05-14 10:07:44 +0000

    Implemented Arudino

--- Main.py
+++ Main.py
@@ -1,66 +1,84 @@
 # List running python 
 # ps -ef | grep python
 
+
+# Set up HTTP Server:
 from server import run_server
-import sqlite3
 import threading
-import RPi.GPIO as GPIO
-import time
-import os
 
 server_thread = threading.Thread(target=run_server)
 server_thread.start()
 
-PulP = 8
+
+import sqlite3
+import RPi.GPIO as GPIO
+import serial
+import time
+import os
+
+# Setup GPIO Pins
+DirP = 3
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)    
-GPIO.setup(PulP,GPIO.OUT)
+GPIO.setup(DirP,GPIO.OUT)
 
+# Open connection to SQLite Database
 sqliteConnection = sqlite3.connect('my_database.db')
-# Create a cursor object
 cursor = sqliteConnection.cursor()
 
-init = 1
+# Check Stepper Driver.
 PulsePerRotation = 1000 * 2
-try:
-    if init == 1:
-        cursor.execute('SELECT Current FROM RPM')
-        Current_RPM = cursor.fetchone()[0]
-        if Current_RPM == 0:
-            Current_RPM = 0.1
-        SecondsPerRotation = 60/(Current_RPM*10.0)
-        SecondsPerPulse = SecondsPerRotation/PulsePerRotation
-        init = 0
-        os.system('uhubctl -l 1-1 -p 2 -a 1')
-        
-    while 1:
+
+# Get the L
+# cursor.execute('SELECT Current FROM RPM')
+# Current_RPM = cursor.fetchone()[0]
+# if Current_RPM == 0:
+
+# Always start at very low RPM
+Current_RPM = 0.1
+cursor.execute("UPDATE RPM SET Current = ? WHERE ID = ?", (0,1))
+Desired_RPM = 0
+SecondsPerRotation = 60/(Current_RPM*10.0)
+SecondsPerPulse = SecondsPerRotation/PulsePerRotation
+MiliSecondsPerPulse = SecondsPerPulse * 1000
+# Initialize Direction Pin.
+GPIO.output(DirP,GPIO.HIGH)
+
+# Turn on power to USB - Turn on fans
+os.system('uhubctl -l 1-1 -p 2 -a 1')
+time.sleep(5)
+# Setup Arduino
+arduino = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=.1)
+
+# Run until keyboard interupt
+try:        
+    while True:
         cursor.execute('SELECT Current FROM RPM')
         Current_RPM = cursor.fetchone()[0]
         cursor.execute('SELECT Desired FROM RPM')
         Desired_RPM = cursor.fetchone()[0]
 
-        # handle if Desired_RPM == 0
-        if Desired_RPM != 0:
-            if Desired_RPM != Current_RPM:
+        if Desired_RPM != Current_RPM:
+            if Desired_RPM != 0:
                 print("Received Desired RPM: ", Desired_RPM)
                 SecondsPerRotation = 60/(Desired_RPM*10.0)
                 SecondsPerPulse = SecondsPerRotation/PulsePerRotation # Delay between each Pulse
-                print("Seconds per pulse: ", SecondsPerPulse)
-                cursor.execute("UPDATE RPM SET Current = ? WHERE ID = ?", (Desired_RPM,1))
-                sqliteConnection.commit()
-            GPIO.output(PulP,GPIO.HIGH)
-            time.sleep(SecondsPerPulse)
-            GPIO.output(PulP,GPIO.LOW)
-            time.sleep(SecondsPerPulse)
-        else:
-            if Desired_RPM != Current_RPM:
-                cursor.execute("UPDATE RPM SET Current = ? WHERE ID = ?", (Desired_RPM,1))
-                sqliteConnection.commit()
-            time.sleep(1)
+                MiliSecondsPerPulse = int(SecondsPerPulse * 1000000)
+                print("Sending MiliSecondsPerPulse to Arduino: ", MiliSecondsPerPulse)
+                print(str(MiliSecondsPerPulse))
+                arduino.write(bytes(str(MiliSecondsPerPulse), 'utf-8'))
+            else:
+                # Send 0 to Arduino
+                arduino.write(bytes(str(0), 'utf-8'))
+            cursor.execute("UPDATE RPM SET Current = ? WHERE ID = ?", (Desired_RPM,1))
+            sqliteConnection.commit()
+        time.sleep(1)
+
 except KeyboardInterrupt:
     sqliteConnection.close()
     os.system('uhubctl -l 1-1 -p 2 -a 0')
     GPIO.cleanup()
+    
 # # subprocess.run([python],)
 
 # import sqlite3
--- PigMotorTest.py
+++ PigMotorTest.py
@@ -2,9 +2,11 @@
 import RPi.GPIO as GPIO
 import time
 
+DirP = 3
 PulP = 8
+
 print('Insert Desired RPM: ')
-RotationsPerMinute = float(input())
+RotationsPerMinute = 100 #float(input())
 
 PulsePerRotation = 1000 * 2
 SecondsPerRotation = 60/RotationsPerMinute 
@@ -13,6 +15,9 @@ SecondsPerPulse = SecondsPerRotation/PulsePerRotation # Delay between each Pulse
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)    
 GPIO.setup(PulP,GPIO.OUT)
+GPIO.setup(DirP,GPIO.OUT)
+
+GPIO.output(DirP,GPIO.LOW)
 
 while 1:
     GPIO.output(PulP,GPIO.HIGH)
